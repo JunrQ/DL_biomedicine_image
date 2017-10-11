f3ef34ef3278ba474bb97433b56f0f7dac89e4a3
JunrQ <137556741@qq.com>
2017-10-11 10:18:54 +0800

    add inference

--- file_path.py
+++ file_path.py
@@ -1,10 +1,14 @@
+import os
+
 # where to save .pkl, after concatnate images
 PKL_PATH = r'E:\zcq\codes\pkl'
 
-# where to save dataset: self.raw_dataset, self.vocab, after load_data
-RAW_DATASET_PATH = r'E:\zcq\codes\weakcnn\theano\top-60-split\raw_dataset.pkl'
+PARENT_PATH = r'E:\zcq\codes\weakcnn\theano\6-10'
 
-VALID_DATASET_PATH = r'E:\zcq\codes\weakcnn\theano\top-60-split\valid_dataset.pkl'
+# where to save dataset: self.raw_dataset, self.vocab, after load_data
+RAW_DATASET_PATH = os.path.join(PARENT_PATH, 'raw_dataset.pkl')
+
+VALID_DATASET_PATH =  os.path.join(PARENT_PATH, 'valid_dataset.pkl')
 
 # saved image parent path, parent_path + gene_stage + .format, give the image file path
 DATASET_PAR_PATH = PKL_PATH
@@ -18,15 +22,9 @@ IMAGE_PARENT_PATH = r'E:\pic_data'
 # tensorflow ckpt file, trained weight file
 CKPT_PATH = r'E:\zcq\codes\weakcnn\theano\vgg_16.ckpt'
 
-'''TOP 20 labels saved path
-# save path
-SAVE_PATH = r'E:\zcq\codes\weakcnn\theano\model.ckpt'
-
-# saved trained ckpt
-MODEL_CKPT_PATH = r'E:\zcq\codes\weakcnn\theano\model.ckpt-13332'
-'''
-
 # it is TOP 60
-SAVE_PATH = r'E:\zcq\codes\weakcnn\theano\top-60-split\model_top60.ckpt'
-MODEL_CKPT_PATH = r'E:\zcq\codes\weakcnn\theano\top-60-split\model_top60.ckpt-5000'
+SAVE_PATH = os.path.join(PARENT_PATH, 'model.ckpt')
+MODEL_CKPT_PATH = None
+# MODEL_CKPT_PATH = r'E:\zcq\codes\weakcnn\theano\6-10\model.ckpt-5000'
 
+SAVE_RESULT_PATH = op.path.join(PARENT_PATH, 'result.pkl')
--- inference.py
+++ inference.py
@@ -1,23 +1,32 @@
 """
 Inference the mode
+
+Performance Evaluation Criteria:
+    1) criteria that are directly extended from traditional single-label
+        evaluation measures to multi-labels [16], [23], [24],
+        such as the *macro F1*, *micro F1*, and *AUC (the Area Under ROC Curve)*;
+    2) criteria that are specifically designed for the setting of multi-labels [48], [56],
+        such as the *hamming loss*, *one-error*, *coverage*, *average precision*,
+        and *ranking loss*.
+    exclude *hamming loss*
+
+
 """
 
 from model import Model
 import ops
 import image_processing
 
-import datetime
 import numpy as np
-import os
-import time
-
 import tensorflow as tf
+from sklearn.metrics import average_precision_score, f1_score, roc_auc_score,\
+                            auc, coverage_error, label_ranking_loss
 
-import itertools
 
 from file_path import *
 
 import math
+import pickle
 
 def sigmoid(x):
   return 1 / (1 + math.exp(-x))
@@ -35,7 +44,9 @@ def main(initial_learning_rate=0.001,
          concatenate_input=False,
          weight_decay=0.00004,
          predict_way='batch_max',
-         top_k_accuracy=3
+         top_k_accuracy=3,
+         threshold=0.8,
+         calculusN=100
           ):
   """
   Args:
@@ -84,18 +95,18 @@ def main(initial_learning_rate=0.001,
       data_num = 0
       data_total_num = len(model.valid_dataset)
 
-      accuracy = 0
-      total_sample = 0
+      # accuracy = 0
+      # total_sample = 0
+
+      y_true = [] # shape [n_samples, n_classes]
+      y_score = [] # shape [n_samples, n_classes]
 
       with tf.Session() as sess:
         print("Number of classes: %d"%model.classes_num)
         sess.run(init)
-        # model.init_fn(sess)
         model.model_init_fn(sess)
 
         for x_step in range(max_steps + 1):
-          # print(single_data)
-          # read in images
           while True:
             if data_num >= data_total_num:
               break
@@ -104,13 +115,10 @@ def main(initial_learning_rate=0.001,
             i = ops.read_image_from_single_file(single_data['filename'])
             l = single_data['label_index']
             if not isinstance(i, int):
-              # i = np.reshape(i, (-1, model.height, model.width))
               i = i[None, :]
               l = l[None, :]
               break
           if model.concatenate_input == True:
-            # !!!!NOTE!!!!:
-            # for the fact shape might not be the same, this way might not work
             raise ValueError
           else:
             i = np.reshape(i, (-1, model.height, model.width, model.channels))
@@ -121,7 +129,6 @@ def main(initial_learning_rate=0.001,
                                             rescale=None,
                                             whiten=False,
                                             normalize=False)
-              # print(i_new[tmp])
             i = i_new
 
           sess.run(model.assing_is_training_false_op)
@@ -138,37 +145,130 @@ def main(initial_learning_rate=0.001,
                             feed_dict={model.images: i,
                                        model.targets: l})
 
-            print(prob[1].shape, prob[2].shape)
-            # print(i)
-            print(prob[1], '\n', prob[4], '\n', prob[5], '\n', prob[-1], prob[-2])
+            # print(prob[1].shape, prob[2].shape)
+            # print(prob[1], '\n', prob[4], '\n', prob[5], '\n', prob[-1], prob[-2])
 
             for single_batch in range(len(prob[0])):
+              #
+              y_true.append(l[single_batch])
+              y_score.append(prob[1][single_batch])
+
               target = ''
               prediction = ''
               pred_result = np.argsort(prob[0][single_batch])
-              # print(prob[0].shape)
-              # print(l[single_batch] == 1)
-              # print(type(model.vocab))
-              # print(pred_result)
 
               for s in vocab[l[single_batch] == 1.]:
                 target += (s + ' \n')
               print('Target: %s' % target)
 
               for s in range(num_pred):
-                # print(pred_result)
                 prediction += (str(vocab[pred_result[-(s+1)]]) + ' \n')
               print('Prediction: %s' % prediction)
 
+              '''
               total_sample += 1
               tmp_t = np.sum(l[0])
               tmp_acc =  (np.sum(l[0][pred_result[-1:-(1+tmp_t):-1]])/ tmp_t)
               accuracy += tmp_acc
 
               print("Current accuracy:  ", tmp_acc)
+              '''
 
 
-      print("Average accuracy: ", accuracy / total_sample)
+      # print("Average accuracy: ", accuracy / total_sample)
+
+      y_true = np.array(y_true)
+      y_score = np.array(y_score)
+      f1_micro = 0.0
+      f1_macro = 0.0
+      assert model.classes_num != y_true.shape[1]
+
+      average_precision = 0.0
+      average_precision = average_precision_score(y_true, y_score)
+
+      # f1 score
+      y_pred = np.where(y_score >= threshold, 1, 0)
+      sk_f1_micro = f1_score(y_true, y_pred, average='micro')
+      sk_f1_macro = f1_score(y_true, y_pred, average='macro')
+
+      TP = np.multiply(y_true, y_pred)
+      FP = np.multiply(1 - y_true, y_pred)
+      FN = np.multiply(y_true, 1 - y_pred)
+      precision = 1.0 * np.sum(TP) / (np.sum(FP) + np.sum(TP))
+      recall = 1.0 * np.sum(TP) / (np.sum(TP) + np.sum(FN))
+      f1_micro += (2.0 * precision * recall / (precision + recall))
+
+      for idx in range(y_true.shape[1]):
+        y_true_ = y_true[:, idx]
+        y_pred_ = y_pred[:, idx]
+        tp = np.multiply(y_true_, y_pred_)
+        fp = np.multiply(1 - y_true_, y_pred_)
+        fn = np.multiply(y_true_, 1 - y_pred_)
+        precision_ = 1.0 * np.sum(tp) / (np.sum(fp) + np.sum(tp))
+        recall_ = 1.0 * np.sum(tp) / (np.sum(tp) + np.sum(fn))
+        f1_macro += (2.0 * precision_ * recall_ / (precision_ + recall_))
+
+      '''
+      for threshold in np.linspace(0.0, 1.0, num=calculusN, endpoint=False):
+        y_pred = np.where(y_score >= threshold, 1, 0)
+        TP = np.multiply(y_true, y_pred)
+        FP = np.multiply(1 - y_true, y_pred)
+        FN = np.multiply(y_true, 1 - y_pred)
+        precision = 1.0 * np.sum(TP) / (np.sum(FP) + np.sum(TP))
+        recall = 1.0 * np.sum(TP) / (np.sum(TP) + np.sum(FN))
+        f1_micro += (2.0 * precision * recall / (precision + recall) / calculusN)
+
+        for idx in range(y_true.shape[1]):
+          y_true_ = y_true[:, idx]
+          y_pred_ = y_pred[:, idx]
+          tp = np.multiply(y_true_, y_pred_)
+          fp = np.multiply(1 - y_true_, y_pred_)
+          fn = np.multiply(y_true_, 1 - y_pred_)
+          precision_ = 1.0 * np.sum(tp) / (np.sum(fp) + np.sum(tp))
+          recall_ = 1.0 * np.sum(tp) / (np.sum(tp) + np.sum(fn))
+          f1_macro += (2.0 * precision_ * recall_ / (precision_ + recall_) / calculusN)
+      '''
+
+      f1_macro /= model.classes_num
+
+      # AUC
+      sk_auc = roc_auc_score(y_true, y_score)
+
+      TPR = []
+      FPR = []
+      for threshold_ in np.linspace(0.0, 1.0, num=calculusN, endpoint=False):
+        tmp_y_pred = np.where(y_score >= threshold_, 1, 0)
+        TP = np.multiply(y_true, tmp_y_pred)
+        FP = np.multiply(1 - y_true, tmp_y_pred)
+        FN = np.multiply(y_true, 1 - tmp_y_pred)
+        TN = np.multiply(1 - y_true, 1 - tmp_y_pred)
+        TPR.append(1.0 * np.sum(TP) / (np.sum(FN) + np.sum(TP)))
+        FPR.append(1.0 * np.sum(FP) / (np.sum(FP) + np.sum(TN)))
+
+      auc_score = auc(FPR, TPR)
+
+      sk_coverage = coverage_error(y_true, y_score)
+      sk_ranking_loss = label_ranking_loss(y_true, y_score)
+
+
+      max_idx = np.argmax(y_score, axis=1)
+
+      one_error = np.sum(np.where(y_true[zip(range(len(max_idx)), max_idx)]==1, 1, 0))
+
+      result = {'f1 micro': f1_micro,
+                'f1 macro': f1_macro,
+                'sk f1 micro': sk_f1_micro,
+                'sk f1 macro': sk_f1_macro,
+                'coverage': sk_coverage,
+                'ranking loss': sk_ranking_loss,
+                'one error': one_error,
+                'sk auc': sk_auc,
+                'auc': auc_score}
+
+      with open(SAVE_RESULT_PATH, 'wb') as f:
+        pickle.dump(result, f, True)
+
+      print(result)
 
 
 if __name__ == '__main__':
