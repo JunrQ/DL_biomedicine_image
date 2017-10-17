fix softmax bug; tweak data flow

models/image_utils.py
@@ -52,13 +52,14 @@ def extract_feature(images, is_training, weight_decay):
     # add new conv layers
     with tf.variable_scope('custom_cnn'):
         with slim.arg_scope(resnet_arg_scope(use_batch_norm=False)):
-            with slim.arg_scope([slim.batch_norm], is_training=is_training):
-                conv = slim.conv2d(feature, 512, (3, 3), stride=2, scope='conv1')
-                bn = slim.batch_norm(conv, scope='batch_norm1')
-                conv = slim.conv2d(bn, 512, (3, 3), stride=1, scope='conv2')
-                bn = slim.batch_norm(conv, scope='batch_norm2')
-                conv = slim.conv2d(bn, 512, (3, 3), stride=1, scope='conv3')
-                bn = slim.batch_norm(conv, scope='batch_norm3')
+            with slim.arg_scope([slim.conv2d], weights_regularizer=slim.l2_regularizer(weight_decay)):
+                with slim.arg_scope([slim.batch_norm], is_training=is_training):
+                    conv = slim.conv2d(feature, 512, (3, 3), stride=2, scope='conv1')
+                    bn = slim.batch_norm(conv, scope='batch_norm1')
+                    conv = slim.conv2d(bn, 512, (3, 3), stride=1, scope='conv2')
+                    bn = slim.batch_norm(conv, scope='batch_norm2')
+                    conv = slim.conv2d(bn, 512, (3, 3), stride=1, scope='conv3')
+                    bn = slim.batch_norm(conv, scope='batch_norm3')
         avg = tf.reduce_mean(bn, [1, 2], keep_dims=False)
         # recover dims N and T
         _, F = avg.get_shape().as_list()

models/rnn.py
@@ -69,11 +69,14 @@ class MemCell(tf.contrib.rnn.RNNCell):
                                       weights_regularizer=slim.regularizers.l2_regularizer(
                                           self.weight_decay),
                                       scope='fc_read')
+        # [N, T]
         logits = tf.reshape(
             logits, shape=[-1, self._mem_size], name='recover_time_of_logits')
+        exp_logits = tf.exp(logits, name='exp_logits')
         mask = tf.sequence_mask(self.length, self._mem_size)
-        effective_logits = logits * tf.cast(mask, logits.dtype)
-        attention = tf.nn.softmax(effective_logits)
+        effective_exp_logits = exp_logits * tf.cast(mask, logits.dtype)
+        divisor = tf.reduce_sum(effective_exp_logits, keep_dims=True, name='softmax_divisor')
+        attention = effective_exp_logits / divisor
         expanded_attention = tf.tile(tf.reshape(attention, [-1, 1], name='reshape_attention'),
                                      [1, self._feature_size], name='expand_attention')
         weighted = expanded_attention * flatten_mem
@@ -110,10 +113,6 @@ class RNN(ModelDesc):
         self.use_glimpse = config.use_glimpse
         self.cost = None
 
-    def _setup_metrics(self, logits, label):
-        # gave logits a reasonable name, so one can access it easily. (e.g. via get_variable(name))
-        logits = tf.identity(logits, name='logits_export')
-
     def _get_inputs(self):
         """ Required by the base class.
         """
@@ -142,8 +141,8 @@ class RNN(ModelDesc):
                                       weights_regularizer=slim.l2_regularizer(
                                           self.weight_decay),
                                       scope='logits')
-        self._setup_metrics(logits, label)
-        
+        # gave logits a reasonable name, so one can access it easily. (e.g. via get_variable(name))
+        logits = tf.identity(logits, name='logits_export')
         loss = tf.losses.sigmoid_cross_entropy(label, logits,
                                                reduction=tf.losses.Reduction.MEAN, scope='loss')
         add_moving_summary(loss)

utils/data_manager/data_manager.py
@@ -4,11 +4,9 @@
     This module transforms image urls and string tags into tensors.
 """
 from .csv_loader import (load_annot_table, load_image_table)
+import cv2
 from functional import seq
 import numpy as np
-from pathlib import Path
-from scipy.misc import imread
-from skimage.transform import resize
 from .seperation import SeparationScheme
 from sklearn.preprocessing import MultiLabelBinarizer
 from tensorpack.dataflow import (BatchData, CacheData, DataFlow, MapData,
@@ -51,7 +49,7 @@ class DataManager(object):
         self.binarizer = MultiLabelBinarizer(classes=vocabulary)
         self.config = config
 
-    def get_train_stream(self, bs):
+    def get_train_stream(self):
         """ Data stream for training.
 
             A stream is a generator of batches. 
@@ -59,25 +57,22 @@ class DataManager(object):
             don not need to tough it directly.
         """
         stream = self._build_basic_stream(self.train_set)
-        stream = BatchData(stream, batch_size=bs)
         return stream
 
-    def get_validation_stream(self, bs):
+    def get_validation_stream(self):
         """ Data stream for validation.
 
             The data is cached for frequent reuse.
         """
         stream = self._build_basic_stream(self.val_set)
         # validation set is small, so we cache it
-        stream = CacheData(stream)
-        stream = BatchData(stream, batch_size=bs)
+        # stream = CacheData(stream)
         return stream
 
-    def get_test_stream(self, bs):
+    def get_test_stream(self):
         """ Data stream for test.
         """
         stream = self._build_basic_stream(self.test_set)
-        stream = BatchData(stream, batch_size=bs)
         return stream
 
     def recover_label(self, encoding):
@@ -99,25 +94,31 @@ class DataManager(object):
         # add length info of image sequence into data points
         stream = MapData(stream, lambda dp: [dp[0], len(dp[0]), dp[1]])
         # read image multithreadedly
+        """
         stream = ThreadedMapData(
-            stream, nr_thread=10,
+            stream, nr_thread=5,
             map_func=lambda dp: [_load_image(
                 dp[0], self.config.image_directory, self.config.image_size),
                 dp[1], dp[2]],
-            buffer_size=40)
+            buffer_size=100)
+        """
+        stream = MapDataComponent(stream, lambda urls: _load_image(
+            urls, self.config.image_directory, self.config.image_size), 0)
         # pad and stack images to Tensor(shape=[T, C, H, W])
         stream = MapDataComponent(stream,
                                   lambda imgs: _pad_input(imgs, self.config.max_sequence_length), 0)
         # one-hot encode labels
         stream = MapDataComponent(stream, self._encode_label, 2)
+        stream = BatchData(stream, batch_size=self.config.batch_size, remainder=True)
         return stream
 
 
 def _load_image(url_list, img_dir, img_size):
     imgs = seq(url_list) \
         .map(lambda url: img_dir + url) \
-        .map(imread) \
-        .map(lambda img: resize(img, img_size, mode='constant')) \
+        .map(lambda loc: cv2.imread(loc, cv2.IMREAD_COLOR)) \
+        .map(lambda img: cv2.cvtColor(img, cv2.COLOR_BGR2RGB)) \
+        .map(lambda img: cv2.resize(img, img_size)) \
         .list()
     return imgs
 